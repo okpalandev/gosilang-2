331a2f63334ed114c955e326f0fab5c5d7288556
Nnamdi Michael Okpala <koodu-platform@protonmail.com>
2024-04-16 01:30:11 +0100

    a

--- gstunit/__init__.py
+++ gstunit/__init__.py
@@ -1,1 +1,17 @@
 
+from .gstunit.__main__ import *
+
+if __name__ == "__main__":
+    # Instantiate the mapping classes
+    standard_layout_mapping = NdebeStandardLayoutMapping()
+    mobile_layout_mapping = NdebeMobileLayoutMapping()
+
+    # Create a dictionary to hold both instances
+    mapping_dict = {
+        'standard_layout_mapping': standard_layout_mapping,
+        'mobile_layout_mapping': mobile_layout_mapping
+    }
+
+    # Pickle the dictionary
+    with open('keyboard_mappings.pkl', 'wb') as f:
+        pickle.dump(mapping_dict, f)
